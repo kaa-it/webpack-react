0d85b90e8940d833e8bd3157c777595f48646e13
Andrey Kruglov <kruglov.it@gmail.com>
2024-11-28 13:46:52 +0300

    step-11 prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/dog.jpg';
-import { ReactComponent as CarolinaLogo } from './assets/images/react.svg';
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
+import { ReactComponent as CarolinaLogo } from './assets/images/react.svg'
 
-console.log(style);
+console.log(style)
 
-const numbers:number[] = [2, 3, 5];
+const numbers: number[] = [2, 3, 5]
 
 // Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+const doubledNumbers = numbers.map((number) => number * 2)
 
-console.log(doubledNumbers); // 4, 6, 10 
+console.log(doubledNumbers) // 4, 6, 10
