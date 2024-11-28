dca81e4ace70bb2e7f11158daaa89836719e3bd5
Andrey Kruglov <kruglov.it@gmail.com>
2024-11-28 12:04:27 +0300

    step-3

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// Импортируем пакет path
+const HTMLWebpackPlugins = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -20,14 +20,18 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
   },
-
-  devServer: {
-      static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-        open: true, // сайт будет открываться сам при запуске npm run dev
-        hot: true,
-  },
+  plugins: [
+    new HTMLWebpackPlugins({
+       template: path.resolve(__dirname, 'public/index.html')
+    }),
+  ],
+//  devServer: {
+//      static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+//        compress: true, // это ускорит загрузку в режиме разработки
+//        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+//        open: true, // сайт будет открываться сам при запуске npm run dev
+//        hot: true,
+//  },
 };
 
 //module.exports — это синтаксис экспорта в Node.js
