fb08720dd23f127ef5f9d2bcd9cac32daab22447
Andrey Kruglov <kruglov.it@gmail.com>
2024-11-28 12:23:36 +0300

    step-5

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,21 @@ module.exports = {
                 },
             },
         ],
-    },
+      },
+      {
+          test: /\.(png|jpg|gif|webp)$/,
+          type: 'asset/resource',
+              generator: {
+                  filename: 'static/images/[hash][ext][query]',
+              },
+      },
+      {
+          test: /\.(woff(2)?|eot|ttf|otf)$/,
+          type: 'asset/resource',
+              generator: {
+                  filename: 'static/fonts/[hash][ext][query]',
+              },
+      }, 
     ],
   },
   resolve: {
